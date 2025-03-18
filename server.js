bba798e07982de6aaf22e46b0bb5bbe3ac4bb0fc
marcant94 <mcantelar@gmail.com>
2025-03-18 17:08:00 +0200

    solo permitimos de stormseekers

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5000;
 
 
 // Permitimos CORS de sitios nuestros
-const allowedOrigins = ['http://localhost:4000', 'https://storm-seekers.gitlab.io'];
+// const allowedOrigins = ['http://localhost:4000', 'https://storm-seekers.gitlab.io'];
+const allowedOrigins = ['https://storm-seekers.gitlab.io'];
 app.use(cors({ origin: allowedOrigins }));
 
 
