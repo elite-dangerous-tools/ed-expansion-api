a986b4702da2e1e3495ad65a026dd17bb2bbc890
marcant94 <mcantelar@gmail.com>
2025-03-18 17:09:00 +0200

    volvemos a habilitar

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 
 
 // Permitimos CORS de sitios nuestros
-// const allowedOrigins = ['http://localhost:4000', 'https://storm-seekers.gitlab.io'];
-const allowedOrigins = ['https://storm-seekers.gitlab.io'];
+const allowedOrigins = ['http://localhost:4000', 'https://storm-seekers.gitlab.io'];
 app.use(cors({ origin: allowedOrigins }));
 
 
