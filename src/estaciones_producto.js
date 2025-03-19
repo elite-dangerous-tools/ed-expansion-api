8367f57e165c81dd498f93483a7fed1f785d5192
marcant94 <mcantelar@gmail.com>
2025-03-19 17:14:00 +0200

    mejorada consulta buscar productos

--- src/estaciones_producto.js
+++ src/estaciones_producto.js
@@ -10,25 +10,26 @@ async function buscarEstacionesProducto(sistemaOrigen, distancia, productos) {
 
     // Permitimos un 110% del valor inicial
     const distanciaInicial = parseInt(distancia);
-    const distanciaPlus = distanciaInicial * 1.1;
+    const distanciaPlus = (distanciaInicial * 1.1).toFixed(2);
 
     const query = `
-        SELECT e."name", e.distance, e."type", pe.id_producto, pe.id_estacion, pe.stock, pe.sellprice
-        FROM estaciones AS e,
-        producto_estacion as pe
-        WHERE e.systemid64 IN 
-        (
-            select s.systemid64
-            from
-                sistemas as s,
-                (SELECT systemid64, nombre, x, y, z FROM sistemas WHERE nombre = '${sistemaOrigen}') as origen
-            where 
-                s.x BETWEEN (origen.x - ${distanciaPlus}) AND (origen.x + ${distanciaPlus}) AND
-                s.y BETWEEN (origen.y - ${distanciaPlus}) AND (origen.y + ${distanciaPlus}) AND
-                s.z BETWEEN (origen.z - ${distanciaPlus}) AND (origen.z + ${distanciaPlus})
-        )
-        and pe.id_producto in (${valores})
-        and pe.id_estacion = e.id
+        SELECT
+            e."name" as estacion, e."type" as tipo, e.distance as distanciaEstacion,
+            pe.id_producto as producto, pe.sellprice as precio, pe.stock as suministro,
+            s.nombre as sistema,
+            sqrt(pow(s.x - origen.x, 2) + pow(s.y - origen.y, 2) + pow(s.z - origen.z, 2)) AS distanciaSistema
+        FROM
+            sistemas AS s
+            JOIN estaciones AS e ON (e.systemid64 = s.systemid64)
+            JOIN producto_estacion AS pe ON (pe.id_estacion = e.id),
+            (SELECT systemid64, nombre, x, y, z FROM sistemas WHERE nombre = '${sistemaOrigen}') AS origen
+        WHERE
+            s.x BETWEEN (origen.x - ${distanciaPlus}) AND (origen.x + ${distanciaPlus})
+        AND s.y BETWEEN (origen.y - ${distanciaPlus}) AND (origen.y + ${distanciaPlus})
+        AND s.z BETWEEN (origen.z - ${distanciaPlus}) AND (origen.z + ${distanciaPlus})
+
+        AND pe.id_producto IN (${valores})
+        ORDER BY pe.id_estacion
     `;
 
     const { rows } = await client.query(query);
