b4971e2aad1c5121becbc28034b693005b150643
marcant94 <mcantelar@gmail.com>
2025-03-31 16:06:00 +0200

    trailblazers con nombre

--- src/distancia_trailblazer.js
+++ src/distancia_trailblazer.js
@@ -9,14 +9,17 @@ client.setTypeParser(20, val => parseInt(val));  // Para BIGINT
 async function buscarDistanciaTrailblazer(sistema) {
     const query = `
             SELECT
-            s.nombre as sistema,
-            sqrt(pow(s.x - origen.x, 2) + pow(s.y - origen.y, 2) + pow(s.z - origen.z, 2)) AS distanciaSistema
+                s.nombre as sistema,
+	            e.name as estacion,
+                sqrt(pow(s.x - origen.x, 2) + pow(s.y - origen.y, 2) + pow(s.z - origen.z, 2)) AS distanciaSistema
             FROM
-            sistemas AS s,
-            (SELECT systemid64, nombre, x, y, z FROM sistemas WHERE nombre = '${sistema}') AS origen
+                sistemas AS s
+	            JOIN estaciones AS e ON (s.systemid64 = e.systemid64),
+                (SELECT systemid64, nombre, x, y, z FROM sistemas WHERE nombre = '${sistema}') AS origen
             WHERE
-            s.nombre in ('HIP 90578', 'Orgen', 'Bletii', '34 Omicron Cephei', 'Klikis', 'Ngobe', 'Minerva')
-            order by 2
+                s.nombre in ('HIP 90578', 'Orgen', 'Bletii', '34 Omicron Cephei', 'Klikis', 'Ngobe', 'Minerva')
+	            and e.name like 'Trailblazer%'
+            ORDER BY 2
     `;
 
     const { rows } = await client.query(query);
