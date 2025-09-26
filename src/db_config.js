874a0f7925dae801853653e0f265aa78bc55c85a
Mario Cantelar <mcantelar@gmail.com>
2025-09-26 16:06:00 +0200

    actualizamos la bbdd

--- src/db_config.js
+++ src/db_config.js
@@ -1,8 +1,8 @@
 
 exports.db_config = {
-    user: "postgres",
+    user: "administrador",
     host: "stormseekers.twilightparadox.com",
     database: "elite",
     password: "8UtsF1FuIskBRt9TTfnM2",
-    port: 5432
+    port: 54321
 };
