f2373c38879aa13f920555ac31aebf213b854b02
marcant94 <mcantelar@gmail.com>
2025-10-15 16:08:00 +0200

    compra venta con sql

--- src/descargar_estaciones.js
+++ src/descargar_estaciones.js
@@ -115,17 +115,22 @@ async function insertarBatchStock() {
     // Hay filas duplicadas que tenemos que descartar
     productosEstaciones = Array.from(new Map(productosEstaciones.map((pe) => [`${pe.id_producto}-${pe.id_estacion}`, pe])).values());
 
-    const valoresProductosEstaciones = productosEstaciones.map((pe) => `('${pe.id_producto}', ${pe.id_estacion}, ${pe.stock}, ${pe.sellPrice})`).join(",");
+    const str_producto = `('${pe.id_producto}', ${pe.id_estacion}, ${pe.stock}, ${pe.sellPrice}, ${pe.demand}, ${pe.buyPrice})`;
+    const valoresProductosEstaciones = productosEstaciones.map((pe) => str_producto).join(",");
     const queryProductosEstaciones = `
-        INSERT INTO producto_estacion (id_producto, id_estacion, stock, sellPrice)
-        SELECT * FROM (VALUES ${valoresProductosEstaciones}) AS temp(id_producto, id_estacion, stock, sellPrice)
+        INSERT INTO producto_estacion (id_producto, id_estacion, stock, sellPrice, demand, buyPrice)
+        SELECT * FROM (VALUES ${valoresProductosEstaciones}) AS temp(id_producto, id_estacion, stock, sellPrice, demand, buyPrice)
         WHERE EXISTS (
             SELECT 1 FROM estaciones WHERE estaciones.id = temp.id_estacion
         )
         ON CONFLICT (id_producto, id_estacion) DO UPDATE
-        SET stock = EXCLUDED.stock,
-        sellPrice = EXCLUDED.sellPrice;
-    `;
+        SET
+        stock = EXCLUDED.stock,
+        sellPrice = EXCLUDED.sellPrice,
+
+        demand = EXCLUDED.demand,
+        sellPrice = EXCLUDED.sellPrice
+    ;`;
 
     try {
         await client.query(queryProductosEstaciones);
