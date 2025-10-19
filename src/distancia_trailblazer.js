16d1ba22c556df7af074f44e75404a2ea0a2c320
Mario Cantelar <mcantelar@gmail.com>
2025-10-19 20:20:02 +0200

    optimizamos

--- src/distancia_trailblazer.js
+++ src/distancia_trailblazer.js
@@ -32,12 +32,34 @@ async function recuperarFiltro(busqueda) {
     return respuesta.results;
 }
 
+async function recuperarBusqueda(sistema) {
+    const parametros = {
+        filters: { name: { value: "Trailblazer" }, type: { value: ["Planetary Outpost", "Mega ship"] } },
+        sort: [{ distance: { direction: "asc" } }],
+        size: 100,
+        page: 0,
+        reference_system: sistema
+    };
+
+    let response = await fetch("https://spansh.co.uk/api/stations/search/", {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify(parametros)
+    });
+
+    let respuesta = await response.json();
+
+    return respuesta.results;
+}
+
 exports.distancia_trailblazer = async (req, res) => {
     try {
         const { sistema } = req.query;
 
-        let busqueda = await solicitarFiltro(sistema);
-        let resultado = await recuperarFiltro(busqueda);
+        let resultado = await recuperarBusqueda(sistema);
 
         let lista_trailblazers = [];
 
