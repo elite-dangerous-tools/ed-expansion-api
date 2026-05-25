74a77609c3a602ea419b91bc6481740a29cd822f
marcant94 <mcantelar@gmail.com>
2026-05-25 16:05:00 +0200

    recuperarBusqueda recursiva

--- src/spansh.js
+++ src/spansh.js
@@ -1,6 +1,4 @@
-
 async function solicitarFiltro(parametros, tipo) {
-
     let response = await fetch("https://spansh.co.uk/api/" + tipo + "/search/save", {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
@@ -26,8 +24,10 @@ async function recuperarFiltro(busqueda, tipo) {
     return respuesta.results;
 }
 
-async function recuperarBusqueda(parametros, tipo) {
-    
+const paginacion = 500;
+const maximoPaginas = 5;
+
+async function llamadaBusqueda(parametros, tipo) {
     let response = await fetch("https://spansh.co.uk/api/" + tipo + "/search/", {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
@@ -37,13 +37,40 @@ async function recuperarBusqueda(parametros, tipo) {
         body: JSON.stringify(parametros)
     });
 
-    let respuesta = await response.json();
+    if (response.status === 200) {
+        let respuesta = await response.json();
+        return respuesta;
+    } else {
+        console.error(response);
+    }
 
-    return respuesta.results;
+    return null;
+}
+
+async function recuperarBusqueda(parametros, tipo) {
+    let repetir = true;
+    parametros.page = 0;
+    parametros.size = paginacion;
+
+    let respuestas = [];
+    while (repetir) {
+        let peticion = await llamadaBusqueda(parametros, tipo);
+
+        if (peticion.count > paginacion && peticion.results.length == paginacion && parametros.page < maximoPaginas) {
+            repetir = true;
+            parametros.page++;
+        } else {
+            repetir = false;
+        }
+
+        respuestas = respuestas.concat(peticion.results);
+    }
+
+    return respuestas;
 }
 
 module.exports = {
     solicitarFiltro: solicitarFiltro,
     recuperarFiltro: recuperarFiltro,
-    recuperarBusqueda: recuperarBusqueda,
+    recuperarBusqueda: recuperarBusqueda
 };
