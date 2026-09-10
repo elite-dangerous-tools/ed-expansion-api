96410b42e5159b23146f4e6effbfb98ee23950d1
Mario Cantelar <mcantelar@gmail.com>
2026-09-10 17:48:23 +0200

    adaptamos para usarlo en local

--- src/db_config.js
+++ src/db_config.js
@@ -1,8 +1,10 @@
 
+require('dotenv').config();
+
 exports.db_config = {
-    user: "apps",
-    host: "mariadb",
-    database: "elite",
-    password: "BOamXj23OmmhDPtOBq1OiWH0e8QFpg",
-    port: 3306
+    user:     process.env.DB_USER     || "apps",
+    host:     process.env.DB_HOST     || "mariadb",
+    database: process.env.DB_DATABASE || "elite",
+    password: process.env.DB_PASSWORD || "BOamXj23OmmhDPtOBq1OiWH0e8QFpg",
+    port:     process.env.DB_PORT     || 3306
 };
